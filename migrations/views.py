9df7065d415685e919b83e24e5c28eaa7f87926e
VebModerator <alexeydyachenko.90@gmail.com>
2025-04-01 21:21:27 +0300

    Update views.py

--- migrations/views.py
+++ migrations/views.py
@@ -20,4 +20,15 @@ def add_post(request):
         return render(request, template_name="blog/index.html", context=context)
 
     if request.method == "POST":
-        post_
+        post_form = PostForm(data=request.Post)
+        if post_form.is_valid():
+            post = post()
+            post.title = post_form.cleaned_data["title"]
+            post.text = post_form.cleaned_data["text"]
+            post.author = post_form.cleaned_data["author"]
+            post.save()
+            return index(request)
+
+
+def read_post(request, pk)
+    post = Post.objects.get(pk=pk)
