eedef26d7ef2304659a9b3c1dccb1cf5f797e2de
VebModerator <alexeydyachenko.90@gmail.com>
2025-04-01 21:39:07 +0300

    Update forms.py

--- migrations/forms.py
+++ migrations/forms.py
@@ -2,7 +2,20 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+#1 способ
 class PostForm(forms.Form):
-    title = forms.Charfield(max_length=200)
-    text = forms.Charfield(widget=forms.Textarea)
-    author = forms.ModelChoiceField(queryset=User.objects.all())
+    title = forms.Charfield(max_length=200, label="Заголовок")
+    text = forms.Charfield(widget=forms.Textarea, label="Текст поста")
+    author = forms.ModelChoiceField(queryset=User.objects.all(), label="Автор")
+
+#2 способ
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+labels = {
+    "title": "Заголовок",
+    "text": "Текст поста"
+}
+    
