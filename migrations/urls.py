bbbfefe9ec7556129e96dc0a825e1362bfb5f8b0
VebModerator <alexeydyachenko.90@gmail.com>
2025-04-01 21:33:29 +0300

    Update urls.py

--- migrations/urls.py
+++ migrations/urls.py
@@ -4,6 +4,7 @@ from blog.views import indexs, about, add_post
 app_name = "blog"
 urlpatterns = [
     path("about/", about, name="about"),
+    path("post/<int:pk>/, read_post, name="read_post")
     path("post/", add_post, name="add_post"),
     path("", index, name="index")
 ]
